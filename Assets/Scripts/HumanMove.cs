5132ec4d526428b230e3cdeec60327c4ab96ebf8
Speedybull <speedybull123@gmail.com>
2017-01-23 20:58:29 -0500

    no message

---
+++ Assets/Scripts/HumanMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMove : MonoBehaviour {
+
+	public float minForce = 20f;
+	public float maxForce = 40f;
+	public float minX = -0.5f;
+	public float maxX = 0.5f;
+	public float minY = -0.5f;
+	public float maxY = 0.5f;
+
+	public Rigidbody2D rb;
+	public float DirectionChange = 0.5f;
+
+	private float directionInterval;
+
+	// Use this for initialization
+	void Start () {
+		directionInterval = DirectionChange;
+
+		rb = GetComponent<Rigidbody2D> ();
+		Push ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		directionInterval -= Time.deltaTime;
+		if (directionInterval < 0) {
+			Push ();
+			directionInterval = DirectionChange;
+		}
+		
+	}
+
+	void Push()
+	{
+		float force = Random.Range (minForce, maxForce);
+		float x = Random.Range (minX, maxX);
+		float y = Random.Range (minY, maxY);
+
+		rb.AddForce (force * new Vector2(x,y));
+	}
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 Target = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Target.z = transform.position.z;
+
+		transform.position = Vector3.MoveTowards (transform.position, Target, speed * Time.deltaTime);
+	}
+}
